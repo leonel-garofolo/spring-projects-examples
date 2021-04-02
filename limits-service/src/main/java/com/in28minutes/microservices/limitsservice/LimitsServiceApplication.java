4fd0045c2e19f9b60565476308b8f46f4c31d122
Leonel <garofolo.leonel@gmail.com>
2021-04-02 16:51:50 -0300

    add config to spring bus

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,6 +2,7 @@ package com.in28minutes.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +10,7 @@ import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableHystrix
+@ConfigurationProperties
 public class LimitsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
